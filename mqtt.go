2d667fef021f50abea6c933871f061e213728536
Stephan Fudeus <sfudeus@users.noreply.github.com>
2024-04-13 00:11:53 +0200

    Add mqtt metrics and improve reconnect handling

--- mqtt.go
+++ mqtt.go
@@ -7,10 +7,33 @@ import (
 	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	log "github.com/sirupsen/logrus"
 )
 
 var mqttClient mqtt.Client
+
+var (
+	gaugeMqttConnected = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "powermeter",
+		Name:      "mqtt_connected",
+		Help:      "Status of the MQTT connection",
+	},
+		[]string{
+			//manual name of the meter, to distinguish between multiple sensors
+			"meter_name",
+		})
+	counterMqttMessages = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "powermeter",
+		Name:      "mqtt_messages",
+		Help:      "Number of MQTT message sent",
+	},
+		[]string{
+			//manual name of the meter, to distinguish between multiple sensors
+			"meter_name",
+		})
+)
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 	log.Info("MQTT connected")
@@ -18,6 +41,7 @@ var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 
 var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
 	log.Warnf("MQTT connection lost: %v", err)
+	gaugeMqttConnected.WithLabelValues(options.MeterName).Set(0)
 }
 
 func connectMqtt() {
@@ -41,6 +65,8 @@ func connectMqtt() {
 	mqttClient = mqtt.NewClient(clientOptions)
 	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
 		log.Errorf("Connect to MQTT failed: %s", token.Error())
+	} else {
+		gaugeMqttConnected.WithLabelValues(options.MeterName).Set(1)
 	}
 }
 
@@ -77,10 +103,16 @@ func publishData(reading meterReading, iteration int) {
 	withDiscoveryData := (iteration%10 == 0)
 	log.Debugf("publishing in iteration %d, with discovery set to %t", iteration, withDiscoveryData)
 
+	if mqttClient == nil || !mqttClient.IsConnected() {
+		log.Info("MQTTClient not initialized or disconnected, reconnecting")
+		connectMqtt()
+	}
+
 	topic := fmt.Sprintf("%s/%s/%s", options.MqttTopicPrefix, options.MeterName, reading.name)
 	if mqttClient != nil && mqttClient.IsConnected() {
 		log.Debugf("Publishing %f to %s", reading.value, topic)
 		t := mqttClient.Publish(topic, 0, false, fmt.Sprintf("%f", reading.value))
+		counterMqttMessages.WithLabelValues(options.MeterName).Inc()
 		go func() {
 			_ = t.Wait() // Can also use '<-t.Done()' in releases > 1.2.0
 			if t.Error() != nil {
